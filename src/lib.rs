7f303927941503f3b9f61a4296882c6a2716e687
pherapont <pherapont@yandex.ru>
2026-04-15 15:11:53 +0700

    first results

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    let mut res = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            res.push(line);
+        }
+    }
+
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
