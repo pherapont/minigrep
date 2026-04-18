1c064a6736c661113460c6b66bb4914ff44abd52
pherapont <pherapont@yandex.ru>
2026-04-18 16:36:13 +0700

    ignore_case searching with envirement variables

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,17 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     res
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut res = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            res.push(line);
+        }
+    }
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
